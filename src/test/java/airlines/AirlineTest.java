2291248608ea90f8a2a8d1cd0004acded5840f1b
marina <marinatofel@gmail.com>
2025-01-28 20:29:31 +0000

    Created test "createAirline" first test

---
+++ src/test/java/airlines/AirlineTest.java
@@ -0,0 +1,33 @@
+package airlines;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class AirlineTest {
+
+    @Test
+    public void createAirline() {
+
+        Response response = RestAssured.given().log().all()
+                .baseUri("https://api.instantwebtools.net/v1/airlines")
+                .contentType(ContentType.JSON)
+                .body("{\n" +
+                        "    \"_id\":\"252d3bca-d9bb-476c-9a97-562d685e235c\",\n" +
+                        "    \"name\": \"Sri Lankan Airways\",\n" +
+                        "    \"country\": \"Sri Lanka\",\n" +
+                        "    \"logo\": \"https://upload.wikimedia.org/wikipedia/en/thumb/9/9b/Qatar_Airways_Logo.svg/sri_lanka.png\",\n" +
+                        "    \"slogan\": \"From Sri Lanka\",\n" +
+                        "    \"head_quaters\": \"Katunayake, Sri Lanka\",\n" +
+                        "    \"website\": \"www.srilankaaairways.com\",\n" +
+                        "    \"established\": \"1990\"\n" +
+                        "}")
+                .post()
+                        .then().log().all().extract().response();
+        Assert.assertEquals(response.statusCode(),200);
+
+    }
+
+}
