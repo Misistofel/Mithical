18e17b8a77e5a7dc3f722e53f5666ea8f9f1c19e
marina <marinatofel@gmail.com>
2025-01-30 21:30:00 +0000

    Was added PayLoads class, RestUtils class and renovated Airline Test class

--- src/test/java/airlines/AirlineTest.java
+++ src/test/java/airlines/AirlineTest.java
@@ -5,27 +5,24 @@ import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import utils.RestUtils;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class AirlineTest {
 
     @Test
     public void createAirline() {
+        String endPoint = "https://api.instantwebtools.net/v1/airlines";
+//        String payload = PayLoads.getCreateAirlinePayloadFromString("252d3bca-d9bb-476c-9a97-562d685e235c", "Sri Lankan Airways",
+//                "Sri Lanka", "https://upload.wikimedia.org/wikipedia/en/thumb/9/9b/Qatar_Airways_Logo.svg/sri_lanka.png",
+//                "From Sri Lanka", "Katunayake, Sri Lanka", "www.srilankaaairways.com", "1990");
 
-        Response response = RestAssured.given().log().all()
-                .baseUri("https://api.instantwebtools.net/v1/airlines")
-                .contentType(ContentType.JSON)
-                .body("{\n" +
-                        "    \"_id\":\"252d3bca-d9bb-476c-9a97-562d685e235c\",\n" +
-                        "    \"name\": \"Sri Lankan Airways\",\n" +
-                        "    \"country\": \"Sri Lanka\",\n" +
-                        "    \"logo\": \"https://upload.wikimedia.org/wikipedia/en/thumb/9/9b/Qatar_Airways_Logo.svg/sri_lanka.png\",\n" +
-                        "    \"slogan\": \"From Sri Lanka\",\n" +
-                        "    \"head_quaters\": \"Katunayake, Sri Lanka\",\n" +
-                        "    \"website\": \"www.srilankaaairways.com\",\n" +
-                        "    \"established\": \"1990\"\n" +
-                        "}")
-                .post()
-                        .then().log().all().extract().response();
+       Map<String, Object> payload= PayLoads.getCreateAirlinePayloadFromMap("252d3bca-d9bb-476c-9a97-562d685e235c", "Sri Lankan Airways",
+                "Sri Lanka", "https://upload.wikimedia.org/wikipedia/en/thumb/9/9b/Qatar_Airways_Logo.svg/sri_lanka.png",
+                "From Sri Lanka", "Katunayake, Sri Lanka", "www.srilankaaairways.com", "1990");
+                Response response = RestUtils.performPost(endPoint,payload, new HashMap<>());
         Assert.assertEquals(response.statusCode(),200);
 
     }
